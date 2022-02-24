f324f7549e4e2b7fdecd044d97597c14853a6b41
moahmed amine mezghich <amine.mezghich@ensi-uma.tn>
2022-02-24 11:18:35 +0100

    projet jdbc operation add avec Scanner + Affichage

---
+++ src/ultjdbc/app/Main.java
@@ -0,0 +1,44 @@
+package ultjdbc.app;
+import java.sql.*;
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) throws SQLException {
+		//System.out.println("Hello JDBC from Java");
+		// STEP 1: on établit une connexion avec la base de données mysql
+	    Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ultjdbc","root","");
+	    //System.out.println(con);
+	    
+	    //Lecture des paramètres à partir du clavier
+	    /*
+	    Scanner sc = new Scanner(System.in);
+	    System.out.println("Donner un nom");
+	    String nom = sc.nextLine();
+	    
+	    System.out.println("Donner la classe");
+	    String classe = sc.nextLine();*/
+	    
+	    
+	   // STEP 2: Création d'un objet statement pour écrire les commandes SQL
+	 	Statement stmt = con.createStatement();
+	    //int result = stmt.executeUpdate("insert into etudiant(nom,classe) values('"+nom+"', '"+classe+"')");
+	    //System.out.println(result);  // ici j'affiche le nombre d'élement insérer
+	    
+	    
+	    // STEP 3: Création objet ResultSET(table en mémoire) dont le rôle est de stocker le résultat d'une requete select
+	 		ResultSet rs = stmt.executeQuery("select id, nom, classe from etudiant");
+	 		
+	 		// STEP 4: On fait le parcours du ResultSet et on affiche son contenu
+	 		
+	 		while(rs.next()) {
+	 			
+	 		int id = rs.getInt("id");
+	 		String nom = rs.getString("nom");
+	 		String classe = rs.getString("classe");
+	 		System.out.println(id+ " "+nom +" "+classe);
+	 		}
+	    System.out.println("Fin du programme");
+
+	}
+
+}
